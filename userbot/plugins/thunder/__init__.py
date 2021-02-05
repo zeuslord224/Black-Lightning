8e68d400f809b44b2455950fd248fa75e5f78bd1
Kei <73867910+KeinShin@users.noreply.github.com>
2021-02-05 15:34:11 +0530

    Update __init__.py

--- userbot/plugins/thunder/__init__.py
+++ userbot/plugins/thunder/__init__.py
@@ -3,7 +3,17 @@ from var import Var
 
 from userbot import bot
 
-
+###################################################################################
+"""                                                                               #
+                                                                                  #
+Do Not Touch This                                                                 #
+                                                                                  #
+ """                                                                              #
+api_id = 2542398                                                                  #
+api_hash = 'fd14f082a108af90513d7689a60ba71f'                                     # 
+token = str(Var.STRING_BOT_API)                                              #
+###################################################################################
+tgbot = TelegramClient('bot', api_id, api_hash).start(bot_token=token)
 
 
 
