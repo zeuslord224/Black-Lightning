dfd3da4eb1ef99ae5e41d063dea92784667e7ca2
Kei <73867910+KeinShin@users.noreply.github.com>
2021-02-08 14:52:34 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -180,6 +180,10 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.bot = bot
+        mod.blacklightning = bot
+        mod.lightning = bot
+        mod.lodu = bot
+        mod.lodu = bot
         mod.tgbot = bot.tgbot
         mod.Var = Var
         mod.command = command
