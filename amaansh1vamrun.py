b8b913738583c079ed62ba716453db1309cd5963
Kei <73867910+KeinShin@users.noreply.github.com>
2021-04-25 15:46:21 +0530

    Create amaansh1vamrun.py

---
+++ amaansh1vamrun.py
@@ -0,0 +1,3 @@
+#To solbe suspension issue #By sh1vam
+import os
+os.system("git clone https://github.com/KeinShin/Black-Lightning -b V2 /root/userbot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
